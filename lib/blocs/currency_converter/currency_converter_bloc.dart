e5bf3e11b2103834219ae6045d8f5c1691efcedd
reonaldi152 <reo@isysedge-soft.com>
2023-04-15 07:05:33 +0700

    register bloc currency converter

--- lib/blocs/currency_converter/currency_converter_bloc.dart
+++ lib/blocs/currency_converter/currency_converter_bloc.dart
@@ -3,15 +3,32 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:ewallet_apps/repository/exception/exception.dart';
 import 'package:ewallet_apps/repository/repository.dart';
+import '../../repository/network/network_data_source.dart';
 import './bloc.dart';
 
 class CurrencyConverterBloc
     extends Bloc<CurrencyConverterEvent, CurrencyConverterState> {
   final Repository _repository;
   CurrencyConverterBloc(Repository repository)
-      : assert(repository != null),
-        _repository = repository,
-        super(CurrencyConverterLoading());
+      : _repository = repository,
+        super(CurrencyConverterLoading()) {
+    on<CurrencyConverterEvent>(((event, emit) async {
+      if (event is GetConvertCurrency) {
+        try {
+          emit(CurrencyConverterLoading());
+
+          final response = await NetworkDataSource()
+              .getConvertCurrency(from: event.from, to: event.to);
+
+          emit(CurrencyConverterLoaded(response!));
+        } on ServerException catch (e) {
+          emit(CurrencyConverterFailure(e.message));
+        } on SocketException catch (e) {
+          print("${e.message},${e.address},${e.port}");
+        }
+      }
+    }));
+  }
 
   @override
   CurrencyConverterState get initialState => CurrencyConverterLoading();
